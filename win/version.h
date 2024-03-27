1405d49a428ef517abcf703d0f6603cb502c0d5d
blueJpg <2238288979@qq.com>
2024-03-28 01:31:49 +0800

    [+] release v0.1.0

--- win/version.h
+++ win/version.h
@@ -2,17 +2,17 @@
 #define VERSION_H
 
 #define PRODUCT_ICON           "icon.ico" // 图标
-#define PRODUCT_NAME           "bitbox" // 产品名称
+#define PRODUCT_NAME           "rssbox" // 产品名称
 
 #define FILE_VERSION           0,1,0   // 文件版本
 #define FILE_VERSION_STR       "0.1.0"
 #define PRODUCT_VERSION        0,1,0   // 产品版本
 #define PRODUCT_VERSION_STR    "0.1.0"
 #define COMPANY_NAME           "Heng30"
-#define INTERNAL_NAME          "bitbox.exe"
-#define FILE_DESCRIPTION       "bitbox is a light bitoin wallet"  // 文件说明
+#define INTERNAL_NAME          "rssbox.exe"
+#define FILE_DESCRIPTION       "rssbox is a RSS client"  // 文件说明
 #define LEGAL_COPYRIGHT        "Copyright 2023-2033 The Heng30 Company Ltd. All rights reserved." // 版权
-#define ORIGINAL_FILE_NAME     "bitbox.exe"    // 原始文件名
+#define ORIGINAL_FILE_NAME     "rssbox.exe"    // 原始文件名
 #define ORGANIZATION_DOMAIN    "https://heng30.xyz"  // 域名
 
 #endif // VERSION_H
